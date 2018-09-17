fe218f810abbfc970364735e92e9a333791b8a2a
Anonymous <anonymous@Anonymouss-MacBook-Pro.local>
2018-09-17 16:58:46 +1000

    downloading

--- src/nxt/peer/Peers.java
+++ src/nxt/peer/Peers.java
@@ -1230,7 +1230,7 @@ public final class Peers {
 
     private static void checkBlockchainState() {
         Peer.BlockchainState state = Constants.isLightClient ? Peer.BlockchainState.LIGHT_CLIENT :
-                (Nxt.getBlockchainProcessor().isDownloading() || Nxt.getBlockchain().getLastBlockTimestamp() < Nxt.getEpochTime() - 600) ? Peer.BlockchainState.DOWNLOADING :
+                (Nxt.getBlockchainProcessor().isDownloading()) ? Peer.BlockchainState.DOWNLOADING :
                         (Nxt.getBlockchain().getLastBlock().getTimestamp() < Nxt.getEpochTime() - (4*60*60) && !Constants.isTestnet) ? Peer.BlockchainState.FORK :
                         Peer.BlockchainState.UP_TO_DATE;
         if (state != currentBlockchainState) {
